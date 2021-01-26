315d10877dace7678124aeaaf1ac07251a1574cc
Daniel <dpericich@gmail.com>
2021-01-25 18:44:22 -0600

    wrote tests to check input type of wrap method's arguments

--- Wrapper.js
+++ Wrapper.js
@@ -1,5 +1,18 @@
+// This Wrapper class accepts no arguments for parameters. It includes a single method called
+// wrap that accepts two arguments inputString {string} and maxColumnLength {int} and return a
+// new string {string}. This string has no lines over the length of maxColumnLength, and where
+// the string would extend past this length, it breaks to a new line.
+
 class Wrapper {
   constructor() {}
+
+  wrap(inputString, maxColumnLength) {
+    if (typeof inputString !== "string") {
+      throw "inputString must be a string";
+    } else if (typeof maxColumnLength !== "number") {
+      throw "maxColumnLength must be a number";
+    }
+  }
 }
 
 module.exports = Wrapper;
--- Wrapper.test.js
+++ Wrapper.test.js
@@ -1,3 +1,22 @@
 const Wrapper = require("./Wrapper");
 
-test("test setup", () => {});
+// Test block to check the input types of our Wrapper class's wrap function.
+// If the input type is not correct, we should expect an error
+
+describe("Both inputs for wrap should be the correct type", () => {
+  const wrapper = new Wrapper();
+  test("the first argument to wrap should be a string", () => {
+    expect(() => wrapper.wrap(7, expect.anything())).toThrow(
+      "inputString must be a string"
+    );
+  });
+  test("the second argument to wrap should be a number", () => {
+    expect(() =>
+      wrapper
+        .wrap("a nice sentence", "an unexpected sentence")
+        .toThrow("maxColumnLength must be a number")
+    );
+  });
+});
+
+// Check that inputs return expected outputs
